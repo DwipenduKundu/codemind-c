a34fde1f1f9c3601080c7a79988a08ded51bc02e
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-15 03:14:45 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,temp,a,count=0,prod=1;     scanf("%d",&n);     temp=n;     while(n!=0)     {         a=n%10;         n=n/10;         count+=a;         prod*=a;     }     if(count==prod)     printf("Spy Number");     else     printf("Not Spy Number");     return 0; }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
-main()
-{int n,i,a,b=0,c=1;
-scanf("%d",&n);
-while(n!=0)
-{a=n%10;
-n=n/10;
-b+=a;
-c*=a;
-}
-if(b==c)
-printf("Spy Number");
-else
-printf("Not Spy Number");
+#include<math.h>
+int main()
+{
+    int n,i,temp,a,count=0,prod=1;
+    scanf("%d",&n);
+    temp=n;
+    while(n!=0)
+    {
+        a=n%10;
+        n=n/10;
+        count+=a;
+        prod*=a;
+    }
+    if(count==prod)
+    printf("Spy Number");
+    else
+    printf("Not Spy Number");
+    return 0;
 }

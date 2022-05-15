fc0741bf46c48be6280733b82718f09a94ef8bf4
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-15 16:00:44 +0530

    #include<stdio.h> int main() {     int i,n,a,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[0]<=arr[i])         arr[0]=arr[i];     }     printf("%d",arr[0]);     return 0; }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[0]<=arr[i])
+        arr[0]=arr[i];
+    }
+    printf("%d",arr[0]);
+    return 0;
+}

ba30026ecc8d83f89e1c41bf08e722bd76a4d24e
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-15 16:26:56 +0530

    #include<stdio.h> int main() {     int i,n,a,arr[100],b;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         b=i;     }     printf("%d",b);     return 0; }

---
+++ Last_even_element_index.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a,arr[100],b;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        b=i;
+    }
+    printf("%d",b);
+    return 0;
+}

d27acb05e8b47ea7cd44ee9568c7d2e0d5324897
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-24 02:32:57 +0530

    #include<stdio.h> int main() {     int a,i,arr[1000],flag=0;     scanf("%d",&a);     for(i=0;i<a;i++)     scanf("%d",&arr[i]);     for(i=0;i<a;i++)     {         if(arr[i]%2==0)         flag=1;         else         {             flag=0;             break;         }     }     if(flag==1)     printf("True");     else     printf("False");     return 0; }

---
+++ Even_Array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,arr[1000],flag=0;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<a;i++)
+    {
+        if(arr[i]%2==0)
+        flag=1;
+        else
+        {
+            flag=0;
+            break;
+        }
+    }
+    if(flag==1)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}

cb3a3777d04dac8dad6c30f9339c645c36a0cf9d
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-24 02:58:24 +0530

    #include<stdio.h> int main() {     int a,i,arr[1000],sum=0;     scanf("%d",&a);     for(i=0;i<a;i++)     scanf("%d",&arr[i]);     for(i=0;i<a;i++)     {         if(arr[i]%2==1)         sum=sum+arr[i];     }     printf("%d",sum);     return 0; }

---
+++ ODD_Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,arr[1000],sum=0;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<a;i++)
+    {
+        if(arr[i]%2==1)
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+}

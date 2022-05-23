2aa0f882c7c889524b0ca0ed005796a2aa321212
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-05-24 02:56:42 +0530

    #include<stdio.h> int main() {     int a,i,arr[1000],sum=0;     scanf("%d",&a);     for(i=0;i<a;i++)     scanf("%d",&arr[i]);     for(i=0;i<a;i++)     {         if(i%2==0)         sum=sum+arr[i];     }     printf("%d",sum);     return 0; }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,arr[1000],sum=0;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<a;i++)
+    {
+        if(i%2==0)
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+}

e488b88dd6a9982c66a331a917b6bc8772808808
DwipenduKundu <97604818+DwipenduKundu@users.noreply.github.com>
2022-09-07 23:52:00 +0530

    #include<stdio.h> int main() {     int n,i,m,j,count=0;     scanf("%d",&n);     scanf("%d",&m);     for(i=n;i<=m;i++)     {         if(i!=1)         {             count=0;             for(j=1;j<i;j++)             {                 if(i%j==0)                 count++;             }             if(count<=1)             printf("%d ",i);         }     }     return 0; }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,m,j,count=0;
+    scanf("%d",&n);
+    scanf("%d",&m);
+    for(i=n;i<=m;i++)
+    {
+        if(i!=1)
+        {
+            count=0;
+            for(j=1;j<i;j++)
+            {
+                if(i%j==0)
+                count++;
+            }
+            if(count<=1)
+            printf("%d
+",i);
+        }
+    }
+    return 0;
+}
